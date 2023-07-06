112d01ae9070db3b6b7841539690e9181481a129
netcat <974123274@qq.com>
2023-07-06 18:26:39 +0000

    hi

--- src/main/java/com.example/Main.java
+++ src/main/java/com.example/Main.java
@@ -1,7 +1,11 @@
 package com.example;
 
+import java.net.InetAddress;
+import java.net.UnknownHostException;
 import java.util.stream.IntStream;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.ComponentScan;
@@ -13,13 +17,26 @@ import org.springframework.web.client.RestTemplate;
 @RestController
 public class Main {
 
+    private final Logger log = LoggerFactory.getLogger(this.getClass());
+
     public static void main(String[] args) {
-        
+
         SpringApplication.run(Main.class, args);
     }
 
     @GetMapping
-    public  String hi(String name) {
+    public String hi(String name) {
+        String workingDirectory = System.getProperty("user.dir");
+        String hostname = "";
+        InetAddress localHost;
+        try {
+            localHost = InetAddress.getLocalHost();
+            log.info("cwd: {}, hostname: {}, ip: {}", workingDirectory, hostname = localHost.getHostName(),
+                    localHost.getHostAddress());
+        } catch (UnknownHostException e) {
+            log.info("cwd: {}, hostname: {}", workingDirectory, "error");
+        }
+
         return "hi" + name;
     }
 }
